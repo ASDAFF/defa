45e4c997e9de5385e5a918c51bbe4f832580fc98
vladzvez <zvezdin.vi@defo.ru>
2019-07-17 11:47:03 +0300

    Перенесен блок с Акцией в карточке. Изменены стили блока Акции в выпадающем меню

--- bitrix/templates/aspro_next_b2c/components/bitrix/catalog.section/catalog_custom_block_new/template.php
+++ bitrix/templates/aspro_next_b2c/components/bitrix/catalog.section/catalog_custom_block_new/template.php
@@ -476,9 +476,9 @@ else
 				</div>
 			</div>
             <div class="col-md-6 col-sm-6 col-xs-6">
-                <div class="another-sale">
+                <!--<div class="another-sale">
                     <a href="">Другие товары акции</a>
-                </div>
+                </div>-->
                 <?if(is_array($arItem["STOCK"]) && $arItem["STOCK"]):?>
                     <div class="stock_wrapper">
                         <?foreach($arItem["STOCK"] as $key => $arStockItem):?>
