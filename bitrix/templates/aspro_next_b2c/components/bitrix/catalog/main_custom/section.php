93b51335f078149120fdfd8aa296a2eb5893937f
Defo production <defo@prod>
2019-08-06 13:28:33 +0300

    восстановление работоспособности разделов

--- bitrix/templates/aspro_next_b2c/components/bitrix/catalog/main_custom/section.php
+++ bitrix/templates/aspro_next_b2c/components/bitrix/catalog/main_custom/section.php
@@ -1270,7 +1270,7 @@ if(!empty($section['UF_SERIES']) && !empty($section["UF_PROS_SERIES"])){
 <?endif;?>
 
     <div class="js_wrapper_items" data-params='<?=str_replace('\'', '"', CUtil::PhpToJSObject($arTransferParams, false))?>'>
-
+	<?@include_once('page_blocks/'.$arParams["SECTION_ELEMENTS_TYPE_VIEW"].'.php');?>
     </div>
 
     <div class="panel-anchors">
